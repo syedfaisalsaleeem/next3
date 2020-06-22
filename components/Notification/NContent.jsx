190b9a71277781bd5ef5fa72d9c1b8bc86e5393e
SyedFaisalSaleeem <syedfaisalsaleem.100@gmail.com>
2020-06-22 21:59:58 +0500

    monday 10:00

--- components/Notification/NContent.jsx
+++ components/Notification/NContent.jsx
@@ -4,6 +4,7 @@ import { makeStyles } from '@material-ui/core/styles';
 import LatestCardN from "./LatestCardN.jsx";
 import LatestCardN1 from "./LatestCardN1.jsx";
 import Link from "next/link";
+import styles from "./NContent.module.css"
 const useStyles = makeStyles((theme) => ({
     root: {
       display: 'flex',
@@ -24,9 +25,9 @@ const useStyles = makeStyles((theme) => ({
 export default function NContent (){
     const classes = useStyles();
     return(
-        <div style={{background:"#E5E5E5"}}>
-            < Grid container   justify="center" spacing={2}>
-                <Grid item xs={12} style={{marginTop:"70px"}}>
+        <div style={{background:"#E5E5E5",width:"98vw"}}>
+            <Grid container   justify="center" spacing={2}>
+                <Grid item xs={11} style={{marginTop:"70px"}}>
 
                 </Grid>
                 <Grid item xs={11}  className={classes.root}>
@@ -41,18 +42,18 @@ export default function NContent (){
                 </Link>
                 </Grid>
             
-                <Grid item xs={11}>
-                    <Grid container direction="row">
-                        <Grid item>
-                            <Typography variant="h4">
+                <Grid item xs={11} style={{margin:"17px"}} >
+                    <Grid container direction="row" spacing={3}>
+                        <Grid item >
+                            <Typography variant="h4" style={{fontSize:"20px",fontStyle:"normal",fontWeight:"500"}}>
                                 Service Updates
                                 
                             </Typography>
                         </Grid>
                         
-                        <Grid item xs={10} >
+                        <Grid item >
                             
-                            <Typography component="div" style={{marginTop:"20px",marginLeft:"20px",height:"2px",background:"black"}}>
+                            <Typography component="div" className={styles.header} >
                                 
                             
                                 </Typography>
@@ -66,26 +67,35 @@ export default function NContent (){
                 </Grid>
                 
                 <Grid item xs={11}>
-                <LatestCardN1/>
-                <LatestCardN1/>
-                <LatestCardN1/>
+                    <Grid container>
+                        <Grid item xs={12}>
+                        <LatestCardN1/>
+                        <LatestCardN1/>
+                        <LatestCardN1/>
+                        </Grid>
+                    </Grid>
+                
                 
                 </Grid>
-                <Grid item xs={11}>
-                    <Grid container direction="row">
-                        <Grid item>
-                            <Typography variant="h4">
+                <Grid item xs={11} style={{margin:"17px",marginTop:"25px"}}>
+                    <Grid container direction="row" spacing={3}>
+                        <Grid item >
+                            <Typography variant="h4" style={{fontSize:"20px",fontStyle:"normal",fontWeight:"500"}}>
                                 Latest Alerts
                                 
                             </Typography>
                         </Grid>
                         
-                        <Grid item xs={10} >
+                        <Grid item  >
                             
-                            <Typography component="div" style={{marginTop:"20px",marginLeft:"20px",height:"2px",background:"black"}}>
+                                
+                                    <Typography component="div" className={styles.header1}  >
+                                    
+                                
+                                    </Typography>
                                 
                             
-                                </Typography>
+                        
                             
                             
                             
@@ -94,16 +104,16 @@ export default function NContent (){
                     </Grid>
 
                 </Grid>
-                <Grid item xs={11}>
+                <Grid item xs={11} >
                 <LatestCardN/>
                 <LatestCardN/>
                 <LatestCardN/>
                
                 </Grid>
-                <Grid item xs={11}>
-                    <Grid container direction="row">
+                <Grid item xs={11} style={{margin:"17px",marginTop:"25px"}}>
+                    <Grid container direction="row" spacing={3}>
                         <Grid item>
-                            <Typography variant="h4">
+                            <Typography variant="h4" style={{fontSize:"20px",fontStyle:"normal",fontWeight:"500"}}>
                                 Support Updates
                                 
                             </Typography>
@@ -111,7 +121,7 @@ export default function NContent (){
                         
                         <Grid item xs={10} >
                             
-                            <Typography component="div" style={{marginTop:"20px",marginLeft:"20px",height:"2px",background:"black"}}>
+                            <Typography component="div" className={styles.header}>
                                 
                             
                                 </Typography>
@@ -131,7 +141,9 @@ export default function NContent (){
                 </Grid>
 
                 
-            
+                <Grid item xs={11} style={{marginTop:"70px"}}>
+
+                </Grid>
             </Grid>
         </div>
     )
